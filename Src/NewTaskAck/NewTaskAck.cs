861f70243ef781a06e3fd4eb998ab22f8c7b79b4
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2019-12-25 21:13:27 -0300

    Task & Work with QoS and Ack

--- Src/NewTaskAck/NewTaskAck.cs
+++ Src/NewTaskAck/NewTaskAck.cs
@@ -3,9 +3,9 @@ using RabbitMQ.Client;
 using System.Text;
 
 
-namespace NewTask
+namespace NewTaskAck
 {
-    class NewTask
+    class NewTaskAck
     {
         static void Main(string[] args)
         {
@@ -13,14 +13,16 @@ namespace NewTask
             using(var connection = factory.CreateConnection())
             using(var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "task_queue", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueDeclare(queue: "task_queue_ack", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
                 var message = GetMessage(args);
                 var body = Encoding.UTF8.GetBytes(message);
 
                 var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
                 channel.BasicPublish(exchange: "",
-                                    routingKey: "task_queue",
+                                    routingKey: "task_queue_ack",
                                     basicProperties: properties,
                                     body: body);
             }
